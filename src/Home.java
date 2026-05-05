f32b79fdc6868a533b452fb5ab8c89210ad1e488
bahana-h <122423688+bahana-h@users.noreply.github.com>
2026-05-05 09:18:05 -0700

    gui first edits

---
+++ src/Home.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+// sources: https://code.tutsplus.com/introduction-to-javafx-for-game-development--cms-23835t
+
+public class Home extends Application {
+    public static void main(String[] arg) {
+        launch(arg);
+    }
+
+    public void start(Stage stagey) {
+        stagey.setTitle("Welcome to Jeopardy!");
+        stagey.show();
+    }
+}
