6831f51ee61ba00a47b15082e81cd99bb4414151
ovoloshyn997-me <ovoloshyn997@student.fuhsd.org>
2026-05-05 09:17:08 -0700

    player

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,62 @@
 public class Player 
 {
     
+public String playerName;
+public int playerScore;
+public boolean isTurn;
+
+
+public Player(String name, int score, boolean turn)
+{
+    playerName = name;
+    playerScore = score;
+    isTurn = turn;
+
+}
+
+
+// methods
+
+public void setScore(int score)
+{
+    playerScore = score;
+}
+
+// can be negative or positive
+public void changeScore( int change)
+{
+    playerScore = playerScore + change;
+}
+
+public void setTurn( boolean turn)
+{
+    isTurn = turn;
+}
+
+public void changeTurn()
+{
+    isTurn = (!isTurn);
+}
+
+
+// getter methods
+
+public String getName()
+{
+    return playerName;
+
+}
+
+public int getScore()
+{
+    return playerScore;
+}
+
+public boolean isTurn()
+{
+    return isTurn;
+}
+
 
     
 }
--- src/Processor.java
+++ src/Processor.java
@@ -1,5 +1,8 @@
 
 import com.opencsv.CSVReader;
+// IT'S NOT RED AT SCHOOL IT WOOOOOORKSSSSS !!!!!!!!!
+// what
+// i cleaned the workspace and it turned red
 public class Processor 
 {
     
