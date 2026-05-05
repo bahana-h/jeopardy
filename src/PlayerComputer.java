62f11f9ba7e5c7002b54e0c6b4f8c18c3d040c0f
ovoloshyn997-me <ovoloshyn997@student.fuhsd.org>
2026-05-05 09:20:58 -0700

    computer player

--- src/PlayerComputer.java
+++ src/PlayerComputer.java
@@ -1,5 +1,11 @@
 
 public class PlayerComputer extends Player
 {
-    
+    public double myPercentage;
+    public PlayerComputer(String name, int score, boolean turn, double percentage)
+    {
+        super(name, score, turn);
+        myPercentage = percentage;
+
+    }
 }
