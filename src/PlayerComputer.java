5a1771784258a400c9f7efb0073de03e906faa0d
SouperFishGlubGlubGlub <mtang863@student.fuhsd.org>
2026-05-12 08:59:07 -0700

    finished player classes

--- src/PlayerComputer.java
+++ src/PlayerComputer.java
@@ -1,11 +1,27 @@
 
 public class PlayerComputer extends Player
 {
-    public double myPercentage;
-    public PlayerComputer(String name, int score, boolean turn, double percentage)
+    public double myPercentageSteal, myPercentageCorrect;
+    public PlayerComputer(String name, int score, boolean turn, double percentageSteal, double percentageCorrect)
     {
         super(name, score, turn);
-        myPercentage = percentage;
+        myPercentageSteal = percentageSteal;
+        myPercentageCorrect = percentageCorrect;
 
     }
+
+    public boolean ifSteal(){
+        int stealVal = (int) (Math.random() * myPercentageSteal) + 1;
+        return (stealVal <= myPercentageSteal);
+
+    }
+
+    public boolean ifCorrect(){
+        int correctVal = (int) (Math.random() * myPercentageCorrect) + 1;
+        return (correctVal <= myPercentageSteal);
+
+    }
+
+
+
 }
