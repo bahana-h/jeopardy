6666886ab0496adab0eb5a9772cbfffb60b2300a
ovoloshyn997-me <ovoloshyn997@student.fuhsd.org>
2026-05-15 21:25:13 -0700

    moving files and preparing for full Maven incorporated operations

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+// PLACEHOLDER
+// TODO: FIX THIS AND MAKE THIS START EVERYTHING
+
+public class Main 
+{
+    public static void main(String[] args) {
+        System.out.println("Starting Jeopardy");
+    }
+}
+
+// confirmed system prints starting jeopardy after running
