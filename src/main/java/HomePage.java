780109282a1e9dc7feedb8bf44e14e3e024fbf12
ovoloshyn997-me <ovoloshyn997@student.fuhsd.org>
2026-05-20 19:35:12 -0700

    unimportant negligible

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,6 +1,14 @@
-import java.io.*;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 // import java.awt.Font;
 // import java.awt.GridBagLayout;
 // import java.awt.Color;
